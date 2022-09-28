68ab2c848fb24eb8e5c56a08bfc3f010953d4e43
Brian Choi <bchoi152@gmail.com>
2022-09-28 09:28:24 -0400

    updated the mistakes

--- Replit Side Projects/passwordStrength.py
+++ Replit Side Projects/passwordStrength.py
@@ -14,6 +14,9 @@ if " " in password:
 elif len(password) < 8:
     print("Password must be at least 8 characters.")
     exit()
+elif "password" in password.lower():
+    print("Really? That's your password?")
+    exit()
 else:
     for char in password:
         if char in symbols:
@@ -24,14 +27,26 @@ else:
             alnumCount += 1
         elif char in numbers:
             numberCount += 1
-    if len(password) < 10 and symbolCount == 0 and numberCount == 0:
+    if len(password) >= 8 and symbolCount == 0 and numberCount == 0:
         print("Very Weak")
-    elif len(password) > 10 and (symbolCount == 0 and numberCount > 0) or (symbolCount > 0 and numberCount == 0):
+    elif len(password) >= 8 and (symbolCount >= 0 and numberCount == 0) or (symbolCount == 0 and numberCount >= 0):
         print("Weak")
-    elif len(password) > 12 and symbolCount > 2 and numberCount > 2:
+    elif len(password) >= 8 and symbolCount >= 2 or numberCount >= 2:
         print("Strong")
-    elif len(password) > 16 and symbolCount > 5 and numberCount > 5:
+    elif len(password) >= 8 and symbolCount >= 3 or numberCount >= 3:
         print("Very Strong")
     else:
-        print("Weak")
+        print("Couldn't determine the strength of the password.")
+
+print(""" 
+Criteria for password strength:
+
+Very Weak: 8 or more characters, no symbols or numbers
+#
+Weak: 8 or more characters, 1 symbol or number
+#
+Strong: 8 or more characters, at least 2 symbols and 2 numbers
+#
+Very Strong: 8 or more characters, at least 3 symbols and 3 numbers
+""")
     
