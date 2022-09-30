54b2cf36b58b2f5dad81cd31964ae79d0252ede8
Brian Choi <bchoi152@gmail.com>
2022-09-30 15:10:39 -0400

    yes

--- Replit Side Projects/currencyConvert.py
+++ Replit Side Projects/currencyConvert.py
@@ -20,20 +20,20 @@ if currency == "1":
 elif currency == "2":
     print(f"{toConvert * 1.37} Canadian Dollars")
 elif currency == "3":
-    print(f"{toConvert * 7.13} Chinese Yuan")
+    print(f"{toConvert * 7.23} Chinese Yuan")
 elif currency == "4":
-    print(f"{toConvert * 1.02} Euros")
+    print(f"{toConvert * 0.85} Euros")
 elif currency == "5":
-    print(f"{toConvert * 81.55} Indian Rupee")
+    print(f"{toConvert * 83.03} Indian Rupee")
 elif currency == "6":
-    print(f"{toConvert * 144.69} Japanese Yen")
+    print(f"{toConvert * 110.49} Japanese Yen")
 elif currency == "7":
-    print(f"{toConvert * 20.16} Mexican Pesos")
+    print(f"{toConvert * 24.24} Mexican Pesos")
 elif currency == "8":
-    print(f"{toConvert * 857.34} Norwegian Kroner")
+    print(f"{toConvert * 10.16} Norwegian Kroner")
 elif currency == "9":
-    print(f"{toConvert * 0.98} Swiss Francs")
+    print(f"{toConvert * 0.97} Swiss Francs")
 elif currency == "10":
-    print(f"{toConvert * 2332} Tanzanian Shillings")
+    print(f"{toConvert * 2600.00} Tanzanian Shillings")
 else:
-    print("Invalid currency.")
+    print("Invalid currency")
