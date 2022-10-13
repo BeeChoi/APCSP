1a923540742f6024e3a0b60c41f46cf07545dce5
Brian Choi <bchoi152@gmail.com>
2022-10-13 09:11:58 -0400

    cringe

--- Decision Statements/triviaQuiz.py
+++ Decision Statements/triviaQuiz.py
@@ -1,95 +1,23 @@
-from time import sleep
-play = "yes"
-points = 0
-guessNum = 0
-while play == "yes":
-    sleep(0.5)
-    print("Hello, and welcome to my very cool trivia game.")
-    sleep(0.5)
-    print("Please enter your answers in lower case letters only.")
-    sleep(0.5)
-    Answer1 = input("What is Avogadro's number? (Express as: Yx10^YY) ")
-    while Answer1 != "6.02x10^23":
-        print("Wrong Answer")
-        guessNum += 1
-        points -= 1
-        Answer1 = input("What is Avogadro's number? (Express as: Yx10^YY) ")
+Answer1 = input("What is Avogadro's number? (Express as: Yx10^YY) ")
+if Answer1 == "6.02x10^23":
     print("Right Answer!")
-    if guessNum > 0:
-        #points -= 1
-        guessNum = 0
-    else:
-        points += 3
-        guessNum = 0
-    sleep(0.5)
-    Answer2 = input("Who was the 40th President of the United States? ") 
-    while Answer2 != "ronald reagan":
-        print("Wrong Answer")
-        guessNum += 1
-        points -= 1
-        Answer2 = input("Who was the 40th President of the United States? ")
+else:
+    print("Wrong Answer!")
+
+Answer2 = input("Who was the 40th President of the United States? ").lower() 
+if Answer2 == "ronald reagan":
     print("Right Answer!")
-    if guessNum > 0:
-        #points -= 1
-        guessNum = 0
-    else:
-        points += 3
-        guessNum = 0
-    sleep(0.5)
-    Answer3 = input("Who is the main character in the movie Catch Me If You Can? ") 
-    while Answer3 != "frank abagnale":
-        print("Wrong Answer")
-        guessNum += 1
-        points -= 1
-        Answer3 = input("Who is the main character in the movie Catch Me If You Can? ")
+else:
+    print("Wrong Answer!")
+
+Answer3 = input("Who is the main character in the movie Catch Me If You Can? ").lower()
+if Answer3 == "frank abagnale":
     print("Right Answer!")
-    if guessNum > 0:
-        #points -= 1
-        guessNum = 0
-    else:
-        points += 3
-        guessNum = 0
-    sleep(0.5)
-    Answer4 = input("What lines have a blue color in the New York City Subway? (separate ONLY with a comma. Ex: 1,2,3) ") 
-    while Answer4 != "a,c,e":
-        print("Wrong Answer")
-        guessNum += 1
-        points -= 1
-        Answer4 = input("What lines have a blue color in the New York City Subway? (separate ONLY with a comma. Ex: 1,2,3) ")
+else:
+    print("Wrong Answer!")
+
+Answer4 = input("What year was the first version of Windows released? ") 
+if Answer4 == "1985":
     print("Right Answer!")
-    if guessNum > 0:
-        #points -= 1
-        guessNum = 0
-    else:
-        points += 3
-        guessNum = 0
-    sleep(0.5)
-    Answer5 = input("What year was the first version of Windows released? ") 
-    while Answer5 != "1985":
-        print("Wrong Answer")
-        guessNum += 1
-        points -= 1
-        Answer5 = input("What year was the first version of Windows released? ")
-    print("Right Answer!")
-    if guessNum > 0:
-        #points -= 1
-        guessNum = 0
-        play = "no"
-    else:
-        points += 3
-        guessNum = 0
-        play = "no"
-if points <= 6:
-    sleep(0.5)  
-    print("You Lost!")
-    sleep(0.5)
-    print("You earned", points, "points.")
-    sleep(0.5)
-    print("Thank you for playing!")
-elif points > 6:
-    sleep(0.5)
-    print("You Won!")
-    sleep(0.5)
-    print("You earned", points, "points.")
-    sleep(0.5)
-    print("Thank you for playing!")
+else:
+    print("Wrong Answer!")
