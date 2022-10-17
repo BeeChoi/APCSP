42d9518a9f35bd6dbccb7c17263c141423b4f147
Brian Choi <bchoi152@gmail.com>
2022-10-17 09:15:51 -0400

    h

--- Decision Statements/triangleTest.py
+++ Decision Statements/triangleTest.py
@@ -1,15 +1,22 @@
-SideOne = int(input("What is the measurement of your first side? "))
-SideTwo = int(input("What is the measurement of your second side? "))
-SideThree = int(input("What is the measurement of your third side? "))
+SideOne = input("What is the measurement of your first side? ")
+SideTwo = input("What is the measurement of your second side? ")
+SideThree = input("What is the measurement of your third side? ")
 
-if (SideOne+SideTwo <= SideThree) or (SideOne+SideThree <= SideTwo) or (SideTwo+SideThree <= SideTwo):
-    print("Not a triangle")
+if SideOne.isalpha() or SideTwo.isalpha() or SideThree.isalpha():
+    print("Error | Invalid measurement(s) given.")
+    exit()
+else:
+    SideOne = float(SideOne)
+    SideTwo = float(SideTwo)
+    SideThree = float(SideThree)
+    if (SideOne+SideTwo <= SideThree) or (SideOne+SideThree <= SideTwo) or (SideTwo+SideThree <= SideTwo):
+        print("Not a triangle")
 
-elif (SideOne == SideTwo) and (SideTwo == SideThree):
-    print("Equilateral Triangle")
+    elif (SideOne == SideTwo) and (SideTwo == SideThree):
+        print("Equilateral Triangle")
 
-elif ((SideOne == SideTwo) and (SideTwo != SideThree)) or ((SideThree == SideTwo) and (SideTwo != SideOne)) or ((SideThree == SideOne) and (SideOne != SideTwo)):
-    print("Isosceles Triangle")
+    elif ((SideOne == SideTwo) and (SideTwo != SideThree)) or ((SideThree == SideTwo) and (SideTwo != SideOne)) or ((SideThree == SideOne) and (SideOne != SideTwo)):
+        print("Isosceles Triangle")
 
-elif (SideOne != SideTwo and SideTwo != SideThree and SideThree !=  SideOne):
-    print("Scalene Triangle")
+    elif (SideOne != SideTwo and SideTwo != SideThree and SideThree !=  SideOne):
+        print("Scalene Triangle")
