19ace52c74676b8cf43e22562e59a49a8c4eb753
Brian Choi <bchoi152@gmail.com>
2022-11-16 09:26:43 -0500

    S

--- Create Task Practice/Part 1/diceSimulator.py
+++ Create Task Practice/Part 1/diceSimulator.py
@@ -22,28 +22,34 @@ def median(sumDice):
     else:
         return sumDice[len(sumDice) // 2]
 
-
 def mode(sumDice):
     return max(set(sumDice), key=sumDice.count)
 
-
-
-
-
-
 def dice_sim(num_rolls, num_dice):
     sumDice = roll_dice(num_rolls, num_dice)
+    if len(sumDice) == 0:
+        sumDice.append(0)
     print("The max is: ", max(sumDice))
     print("The min is: ", min(sumDice))
     print("The mean is: ", mean(sumDice))
     print("The median is: ", median(sumDice))
     print("The mode is: ", mode(sumDice))
-
-    
-    
+  
 def main():   
-    num_rolls = int(input("How many times do you want to roll the dice? "))
-    num_dice = int(input("How many dice do you want to roll? "))
-    dice_sim(num_rolls, num_dice)
+    simnum = 1
+    # num_dice = int(input("How many dice do you want to roll? "))
+    loopAmt = int(input("How many simulations do you want to run? "))
+    for i in range(loopAmt):
+        if simnum == 1:
+            print("\nSimulation #", simnum)
+            print("No Dice Were Rolled.")
+            simnum += 1
+        else:   
+            num_rolls = int(input("\nHow many times do you want to roll the dice? "))
+            print("\nSimulation #", simnum)
+            print("Number of dice rolled: ", (simnum - 1), "Number of rolls: ", num_rolls)
+            dice_sim(num_rolls, simnum - 1)
+            simnum += 1
+        # dice_sim(num_rolls, num_dice)
 
 main()
