0bf7ffbd4f52593cf47f04fcc01350b8cb63937b
Brian Choi <bchoi152@gmail.com>
2022-12-08 09:24:23 -0500

    fdsfhgjkl;'

--- Learn path/HexadecimalToDecimalConversion.py
+++ Learn path/HexadecimalToDecimalConversion.py
@@ -3,6 +3,7 @@ if type == "decimal":
     num = int(input("What number would you like to convert? "))
     hex = hex(num)
     print(hex)
+    print("*Note: 0x denotes that the number is in hexadecimal.")
 elif type == "hexadecimal":
     num = input("What number would you like to convert? ")
     dec = int(num, 16)
