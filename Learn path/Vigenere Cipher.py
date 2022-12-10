3dce035a73ab5416daadc0bb2ef258b8775e5032
Brian Choi <bchoi152@gmail.com>
2022-12-10 13:52:03 -0500

    Vigenere

--- Learn path/Vigenere Cipher.py
+++ Learn path/Vigenere Cipher.py
@@ -1,11 +1,53 @@
-defaultKey = {"a":"a","b":"b","c":"c","d":"d","e":"e","f":"f","g":"g","h":"h","i":"i","j":"j","k":"k","l":"l","m":"m","n":"n","o":"o","p":"p","q":"q","r":"r","s":"s","t":"t","u":"u","v":"v","w":"w","x":"x","y":"y","z":"z"}
+defKey = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+allKeys = []
 
 def GenerateKey(Keyword):
-    alphbet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    keys = []
-    key = {}
-    for i in range(len(Keyword)):
-        for 
+    for i in Keyword:
+        key = []
+        ind = defKey.index(i)
+        for j in range(ind,26):
+            key.append(defKey[j])
+        for j in range(0,ind):
+            key.append(defKey[j])
+        allKeys.append(key)
+    return allKeys
        
+def Encrypt(Keyword,Message):
+    allKeys = GenerateKey(Keyword)
+    EncryptedMessage = ""
+    for i in range(0,len(Message)):
+        if Message[i] == " ":
+            EncryptedMessage += " "
+        else:
+            ind = defKey.index(Message[i])
+            EncryptedMessage += allKeys[i%len(Keyword)][ind]
+    return EncryptedMessage
 
-print(GenerateKey("one"))
+def Decrypt(Keyword,Message):
+    allKeys = GenerateKey(Keyword)
+    DecryptedMessage = ""
+    for i in range(0,len(Message)):
+        if Message[i] == " ":
+            DecryptedMessage += " "
+        else:
+            ind = allKeys[i%len(Keyword)].index(Message[i])
+            DecryptedMessage += defKey[ind]
+    return DecryptedMessage
+
+def main():
+    print("Welcome to Vigenere Cipher")
+    print("1. Encrypt")
+    print("2. Decrypt")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        Keyword = input("Enter the keyword: ")
+        Message = input("Enter the message: ")
+        print("Encrypted message: ",Encrypt(Keyword,Message))
+    elif choice == 2:
+        Keyword = input("Enter the keyword: ")
+        Message = input("Enter the message: ")
+        print("Decrypted message: ",Decrypt(Keyword,Message))
+    else:
+        print("Invalid choice")
+
+main()
