d86f6e06425c2b37df5909adbd5775b66224f02b
Brian Choi <bchoi152@gmail.com>
2022-12-10 12:06:36 -0500

    tffui

--- Learn path/Binary Search.py
+++ Learn path/Binary Search.py
@@ -1,43 +1,31 @@
 import random
 numbers = []
-for i in range(1, 100):
+for i in range(1, 101):
     numbers.append(i)
-#print(numbers)
-
+print(numbers)
 def binary_search():
     personalGuess = int(input("Enter your guess: "))
     highGuess = 101
     lowGuess = 0
     numGuesses = 0
     while numGuesses != 7:
-        guess = random.choice(numbers)
-        print("\nGuess Number: " + str(numGuesses))
-        print("Reminder: Your number is: ", personalGuess)
-        print("I guessed:", guess)
-        input1 = input("Was the guess high, low, or correct? ")
-        if input1 == "high":
-            highGuess = guess
+        print("is your number", numbers[int((len(numbers)/2)-1)], "or above or below? ")
+        CPUguess = input("Enter [above], [below], or [correct]:]")
+        if CPUguess == "above":
+            lowGuess = numbers[int((len(numbers)/2)-1)]
             numbers.clear()
             for i in range(lowGuess + 1, highGuess):
                 numbers.append(i)
-            #print(numbers)
-            #print("Low guess:", lowGuess, "High guess", highGuess)
             numGuesses += 1
-
-        elif input1 == "low":
-            lowGuess = guess
+        elif CPUguess == "below":
+            highGuess = numbers[int((len(numbers)/2)-1)]
             numbers.clear()
             for i in range(lowGuess + 1, highGuess):
                 numbers.append(i)
-            #print(numbers)
-            #print("Low guess:", lowGuess, "High guess", highGuess)
             numGuesses += 1
-
-        elif input1 == "correct":
-            print("I guessed correctly!")
-            exit()
-
+        elif CPUguess == "correct":
+            print("I guessed your number in", numGuesses, "guesses!")
+            break
         else:
             print("Please enter a valid response.")
-
 binary_search()
--- Learn path/HexadecimalToDecimalConversion.py
+++ Learn path/HexadecimalToDecimalConversion.py
@@ -1,10 +1,20 @@
 type = input("Is your number in decimal or hexadecimal? ")
 if type == "decimal":
-    num = int(input("What number would you like to convert? "))
-    hex = hex(num)
-    print(hex)
-    print("*Note: 0x denotes that the number is in hexadecimal.")
+    conversion_table = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A' , 'B', 'C', 'D', 'E', 'F']
+    decimal = int(input("Enter a number: "))
+    hexadecimal = ''
+    while(decimal>0):
+        remainder = decimal%16
+        hexadecimal = conversion_table[remainder]+ hexadecimal
+        decimal = decimal//16
+    print("Hexadecimal: ",hexadecimal)
+    
 elif type == "hexadecimal":
-    num = input("What number would you like to convert? ")
-    dec = int(num, 16)
-    print(dec)
+    conversion_table = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10 , 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+    hexadecimal = input("Enter the hexadecimal number: ").strip().upper()
+    decimal = 0
+    power = len(hexadecimal) -1
+    for digit in hexadecimal:
+        decimal += conversion_table[digit]*16**power
+        power -= 1
+    print(decimal)
