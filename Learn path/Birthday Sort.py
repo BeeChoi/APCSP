deb1fd2afff1a4d6715421916d873c2137e89744
Brian Choi <bchoi152@gmail.com>
2022-12-10 12:06:24 -0500

    gkhjlk;l';'

--- Learn path/Birthday Sort.py
+++ Learn path/Birthday Sort.py
@@ -3,22 +3,37 @@ birthdays = []
 for y in range(10):
     birthdays.append(str(random.randint(1, 12)) + "/" + str(random.randint(1, 31)))
     y += 1
-print(birthdays)
+print("Dates:", birthdays)
 
 def selection_sort():
     newBirthdays = []
-    for i in range(len(birthdays)):
-        newBirthdays.append(min(birthdays))
-        birthdays.remove(min(birthdays))
-    return newBirthdays
+    tmp = []
+    sort = []
+    nbSTR = ""
+    for i in birthdays:
+        i = i.split("/")
+        i[0]=int(i[0])
+        i[1]=int(i[1])
+        tmp.append(i)
+    tmp.sort(key=lambda x: (x[0], x[1]))
+    for x in tmp:
+        nbSTR += str(x[0]) + "/" + str(x[1]) + ", "
+    return nbSTR
 
 def bubble_sort():
-    for i in range(len(birthdays)):
-        for j in range(len(birthdays) - 1):
-            if birthdays[j] > birthdays[j + 1]:
-                birthdays[j], birthdays[j + 1] = birthdays[j + 1], birthdays[j]
-    return birthdays
+    newBirthdays = []
+    tmp = []
+    sort = []
+    nbSTR = ""
+    for i in birthdays:
+        i = i.split("/")
+        i[0]=int(i[0])
+        i[1]=int(i[1])
+        tmp.append(i)
+    tmp.sort(key=lambda x: (x[0], x[1]), reverse=True)
+    for x in tmp:
+        nbSTR += str(x[0]) + "/" + str(x[1]) + ", "
+    return nbSTR
 
-print(selection_sort())
-print("----------------")
-print(bubble_sort())
+print("Selection Sort:", selection_sort())
+print("Bubble Sort:", bubble_sort())
